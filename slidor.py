5afa3bb3a775c2e29f8d7c047a9f18aaf8774407
AdLac <44724937+AdLac@users.noreply.github.com>
2023-02-06 13:11:37 +0100

    Update slidor.py

--- slidor.py
+++ slidor.py
@@ -11,40 +11,6 @@ import streamlit as st
 # OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
 
-#Password
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
-
-if check_password():
-    st.write("Here goes your normal Streamlit app...")
-    st.button("Click me")
-
-
 def main():
     # Titre
     st.title('Slide Generator :sunglasses:')
